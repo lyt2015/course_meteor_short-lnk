5f666e9756592a80864e371661bb975fce1c751d
lyt2015 <lyt2015@users.nonreply.github.com>
2018-08-15 15:56:56 +0800

    Add header style

--- imports/ui/components/PrivateHeader.js
+++ imports/ui/components/PrivateHeader.js
@@ -14,9 +14,13 @@ const PrivateHeader = props => {
   }
 
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <button onClick={handleLogout}>Logout</button>
+    <div className="header">
+      <div className="header__content">
+        <h1 className="header__title">{props.title}</h1>
+        <button className="button button--link-text" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
     </div>
   )
 }
